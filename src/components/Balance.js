9b32e658989b9aa686f27e82faba79b7d44c56de
Alex Santamaria <santamariaalex146@gmail.com>
2021-09-21 17:36:47 -0500

    total bal is dynamically computed

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { useContext } from "react/cjs/react.development";
+import { GlobalContext } from "../context/GlobalState";
 
 export const Balance = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  const amounts = transactions.map((transaction) => transaction.amount);
+  const total = amounts.reduce((acc, item) => (acc += item)).toFixed(2);
+
+  console.log(amounts, total);
+
   return (
     <div>
       <h4>Your Balance</h4>
-      <h1>$0.00</h1>
+      <h1>${total}</h1>
     </div>
   );
 };
